2851a2de2a4cfa63722a580e866279dc5b98f150
AnuragLandge <avlandge77@gmail.com>
2025-01-02 09:24:37 +0530

    first

---
+++ Navale/Dotnetlab/LoginDemoMvc/LoginDemoMvc/Migrations/20250101174835_EmpMigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginDemoMvc.Migrations
+{
+    public partial class EmpMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
